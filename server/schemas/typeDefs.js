22024c858bc8a722e0ccb50050aafe0580bc36d7
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-16 16:54:39 -0400

    completed defining type definitions.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,9 +5,12 @@ const typeDef = gql`
     _id: ID
     username: String
     email: String
-    password: String
     savedBooks: [Book]!
     bookCount: Int
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
   type Book {
     authors: [String]!
@@ -16,5 +19,23 @@ const typeDef = gql`
     image: String
     link: String
     title: String!
+  }
+  input InputBook {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,11 +5,6 @@ const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -30,10 +25,14 @@ module.exports = {
       req.user = data;
     } catch {
       console.log("Invalid token");
-      return res.status(400).json({ message: "invalid token!" });
     }
 
     // send to next endpoint
     next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
