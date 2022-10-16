c575207aec5e9db62a2526e4a6874594247c5066
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-16 17:06:07 -0400

    added me and mutation addUser,login resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,39 @@
 const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, {}, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user.id }).select(
+          "-__v -password"
+        );
+      } else {
+        throw new AuthenticationError("Please log in!");
+      }
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email was found!");
+      }
+      //   User schema has a isCorrectPassword method
+      const password = await user.isCorrectPassword(password);
+      if (!password) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
